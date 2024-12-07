41e50ab09cc619b47b67c46000ee49c365a3b771
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-06 21:45:14 -0500

    User class to store user information, and potentially soon add to an userbase as well

--- nudining-info/src/main/java/com/nudining/nudining_info/entities/user.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/entities/user.java
@@ -1,5 +1,52 @@
 package com.nudining.nudining_info.entities;
 
-public class user {
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private ArrayList<String> dietaryRestrictions;
+    private ArrayList<String> nutritionalFocus;
+
+
+    public User(String name, ArrayList<String> dietaryRestrictions, ArrayList<String> nutritionalFocus){
+        this.name = name;
+        this.dietaryRestrictions = dietaryRestrictions;
+        this.nutritionalFocus = nutritionalFocus;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public ArrayList<String> getDietaryRestrictions(){
+        return this.dietaryRestrictions;
+    }
+    public void addDietaryRestrictions(String newDietaryRestriction){
+        this.dietaryRestrictions.add(newDietaryRestriction);
+    }
+
+    public void removeDietaryRestrictions(String dietaryRestriction){
+        this.dietaryRestrictions.remove(dietaryRestriction);
+    }
+
+    public ArrayList<String> getNutritionalTypes(){
+        return this.nutritionalFocus;
+    }
+
+    public void addNutritionalType(String nutritionalType){
+        this.nutritionalFocus.add(nutritionalType);
+    }
+
+    
+
+
+
     
 }
+
+
+
