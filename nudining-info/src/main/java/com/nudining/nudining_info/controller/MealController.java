df9c56ee22d3ae6f1d5c986fb03c79f82bcb30ff
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-13 21:19:36 -0500

    added a recomend meal controller route, might have to change due to temporary value variables

--- nudining-info/src/main/java/com/nudining/nudining_info/controller/MealController.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/controller/MealController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/meals")
@@ -14,6 +15,12 @@ public class MealController {
 
     @Autowired
     private MealService mealService;
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+                    
+        return user;
+    }
 
     @PostMapping("/generate")
     public ArrayList<Meal> generatePotentialMeals(
@@ -23,5 +30,22 @@ public class MealController {
             @RequestParam ArrayList<String> kitchens) {
         
         return mealService.generatePotentialMeals(user, periods, locations, kitchens);
+    }
+
+    @PostMapping("/recommend")
+    public List<Meal> recommendMeals(
+            @RequestBody User user,
+            @RequestParam ArrayList<String> periods,
+            @RequestParam ArrayList<String> locations,
+            @RequestParam ArrayList<String> kitchens,
+            @RequestParam(required = false) Float maxCalories,
+            @RequestParam(required = false) Float maxFat,
+            @RequestParam(required = false) Float minVitaminD) {
+        
+        // Generate potential meals first
+        ArrayList<Meal> potentialMeals = mealService.generatePotentialMeals(user, periods, locations, kitchens);
+        
+        // Get recommended meals based on nutritional values
+        return mealService.recommendMealsBasedOnNutritionalValues(user, potentialMeals, maxCalories, maxFat, minVitaminD);
     }
 }
