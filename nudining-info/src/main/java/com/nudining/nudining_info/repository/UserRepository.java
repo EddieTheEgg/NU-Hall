58b70fe1627277c742695b2dfa5ff2e533a2375c
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-14 18:50:23 -0500

    Added a possible query for user for now, will need to update this for future services requesting certain user information such as dietary restrictions and nutritonal focus

--- nudining-info/src/main/java/com/nudining/nudining_info/repository/UserRepository.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.nudining.nudining_info.repository;
 
 import com.nudining.nudining_info.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // You can define custom query methods here if needed
-
-    // Example: Find a user by name
-    User findByName(String name);
+    
+    Optional<User> findByEmail(String email);
 }
