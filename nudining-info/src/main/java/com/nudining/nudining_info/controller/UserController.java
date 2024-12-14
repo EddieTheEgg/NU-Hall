eecd7acced2cea22c5834c6c589d6a898bed9b8c
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-14 18:48:47 -0500

    Added some possible routes for now to fetch and add users to database

--- nudining-info/src/main/java/com/nudining/nudining_info/controller/UserController.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/controller/UserController.java
@@ -3,22 +3,32 @@ package com.nudining.nudining_info.controller;
 import com.nudining.nudining_info.entities.User;
 import com.nudining.nudining_info.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // Import for password hashing
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/users") // Changed to /users for clarity
+@RequestMapping("/api/users") // Changed to /users for clarity
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    //User Methods:
+
+    //Create Users
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/addUser")
     public User addUser(@RequestBody User user) {
-    
-        // Save the user to the database
         return userService.saveUser(user);
     }
-}
+    
+    //Find user by ID
+    @GetMapping("/getUser/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.findUserByID(id)
+            .orElseThrow(() -> new RuntimeException("User not found with given id"));
+    }
+
+
+
+} 
