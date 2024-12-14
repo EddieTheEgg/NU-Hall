f1f5f66c1f47ff26e38c7f2d24341cef1e789b47
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-13 21:23:40 -0500

    some example data used for now

--- Data/FetchDailyMenu/dataConverter.py
+++ Data/FetchDailyMenu/dataConverter.py
@@ -6,7 +6,7 @@ locations = ["Stetson_East", "Stetson_West", "IV"]
 periods = ["Breakfast", "Lunch", "Dinner"]
 
 # Test Cases
-date = '2024-12-5'
+date = '2024-12-7'
 
 # Combine all dining options into one CSV file
 all_menu_items = []  # List to hold all menu items
