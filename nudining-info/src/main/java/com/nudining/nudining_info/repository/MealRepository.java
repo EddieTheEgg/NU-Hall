5492785913e850f5ef1e07da25ea245e5627ef4a
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-16 18:35:32 -0500

    deleted unecesary queries, and just have this query to get specific types of items from the database of daily_menu

--- nudining-info/src/main/java/com/nudining/nudining_info/repository/MealRepository.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/repository/MealRepository.java
@@ -10,12 +10,13 @@ import java.util.ArrayList;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
-  
-    @Query("SELECT m FROM Meal m WHERE m.period IN (:periods)")
-    ArrayList<Meal> findMealsByPeriods(@Param("periods") ArrayList<String> periods);
 
-    @Query("SELECT m from Meal m WHERE m.period IN (:periods) AND m.location IN (:locations) AND m.kitchen IN (:kitchens)")
-    ArrayList<Meal> findMealsByPeriodsLocationsKitchens(@Param ("periods")ArrayList<String> periods, @Param("locations") ArrayList<String> locations, @Param("kitchens") ArrayList<String> kitchens);
+    @Query("SELECT m FROM Meal m WHERE m.period IN (:periods) AND m.location IN (:locations) AND m.kitchen IN (:kitchens)")
+    ArrayList<Meal> findMealsByPeriodsLocationsKitchens(
+        @Param("periods") ArrayList<String> periods, 
+        @Param("locations") ArrayList<String> locations, 
+        @Param("kitchens") ArrayList<String> kitchens
+    );
     
 
     
