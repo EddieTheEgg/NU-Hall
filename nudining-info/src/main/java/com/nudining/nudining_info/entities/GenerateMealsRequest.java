8a0c8f25e5c3d732d92d3e6e888bc061beb8e314
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-16 18:33:33 -0500

    A class to take in a user one-time generate meal request and turn into a java class for processing

---
+++ nudining-info/src/main/java/com/nudining/nudining_info/entities/GenerateMealsRequest.java
@@ -0,0 +1,72 @@
+package com.nudining.nudining_info.entities;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class GenerateMealsRequest {
+    private User user; //We will always have user information sent to intiliaze this for each instance
+    private ArrayList<String> periods;
+    private ArrayList<String> locations;
+    private ArrayList<String> kitchens;
+
+    public GenerateMealsRequest(User user, ArrayList<String> periods, ArrayList<String> locations, ArrayList<String> kitchens){
+        this.user = user;
+        this.periods = periods;
+        this.locations = locations;
+        this.kitchens = kitchens;
+    }
+
+    //Methods
+
+    //user
+    public User getUser(){
+        return user;
+    }
+    public void setUser(User newUserInfo){
+        this.user = newUserInfo;
+    }
+
+    //periods
+    public ArrayList<String> getPeriods(){
+        return periods;
+    }
+    public void setPeriods(ArrayList<String> newPeriods){
+        this.periods = newPeriods;
+    }
+
+    //locations
+    public ArrayList<String> getLocations(){
+        return locations;
+    }
+    public void setLocations(ArrayList<String> newLocations){
+        this.locations = newLocations;
+    }
+
+    //kitchens
+    public ArrayList<String> getKitchens(){
+        return kitchens;
+    }
+    public void setKitchesn(ArrayList<String> newKitchens){
+        this.kitchens = newKitchens;
+    }
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
