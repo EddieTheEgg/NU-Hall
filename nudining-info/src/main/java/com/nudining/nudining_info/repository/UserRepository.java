7213063036d8145ba5040d3d654f2c326a693c3d
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-18 18:19:38 -0500

    removed unecessary import

--- nudining-info/src/main/java/com/nudining/nudining_info/repository/UserRepository.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.nudining.nudining_info.repository;
 
 import com.nudining.nudining_info.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
