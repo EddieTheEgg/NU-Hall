45e61bb0eab3054ae8597e86b251bf15762e0f14
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-20 13:58:28 -0500

    Also for login to give the frontend a route to POST

--- nudining-info/src/main/java/com/nudining/nudining_info/controller/UserController.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/controller/UserController.java
@@ -2,16 +2,26 @@ package com.nudining.nudining_info.controller;
 
 import com.nudining.nudining_info.entities.User;
 import com.nudining.nudining_info.service.UserService;
+import com.nudining.nudining_info.entities.UserLoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     //User Methods:
 
@@ -36,6 +46,21 @@ public class UserController {
     public User getUserByEmail(@PathVariable String email) {
         return userService.findUserByEmail(email)
             .orElseThrow(() -> new RuntimeException("User not found with given email"));
+    }
+
+    //Validate user attempt with login
+    @PostMapping("/login")
+    @CrossOrigin(origins = "http://localhost:5173")
+    public ResponseEntity<String> login(@RequestBody UserLoginRequest loginRequest) {
+        Optional<User> user = userService.findUserByEmail(loginRequest.getEmail());
+        
+        if (user.isPresent() && passwordEncoder.matches(loginRequest.getPassword(), user.get().getPassword())) {
+            // Return success response
+            return ResponseEntity.ok("Login successful");
+        } else {
+            // Return failure response
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email or password mismatch");
+        }
     }
 
 } 
