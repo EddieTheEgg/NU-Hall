ebbd243ac0bd609e84bd6f0881f7edbe326677e5
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2024-12-25 11:18:36 -0500

    added a template for now homepage dashboard

--- NU-Dining/src/components/HomePage.jsx
+++ NU-Dining/src/components/HomePage.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useSignup } from '../context/SignupContext.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+    const { signupData } = useSignup();
+    const navigate = useNavigate();
+
+    const handleMakeMealClick = () => {
+        navigate('/meal-preferences');
+    };
+
     return (
-        <div>
-            <h2>Welcome to the Home Page!</h2>
-            {/* Add your homepage content here */}
-        </div>
+        <>
+            <section>
+                <h1>Create a meal plan:</h1>
+                <button type="button" onClick={handleMakeMealClick}>Make a meal!</button>
+            </section>
+            <div>
+                {signupData.name && <p>Hello, {signupData.name}!</p>}
+                {/* Add more user-specific content here */}
+            </div>
+        
+        </>
+        
     );
 };
 
