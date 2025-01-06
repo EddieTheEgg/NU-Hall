4080436f176cab2b3f80dfb075a22b022e417ec3
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-01-05 21:01:37 -0500

    Added a controller route for updating nutrition goals, set up the format with a Range object for every key pair value

--- nudining-info/src/main/java/com/nudining/nudining_info/controller/UserController.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.nudining.nudining_info.controller;
 
 import com.nudining.nudining_info.entities.User;
+import com.nudining.nudining_info.entities.Range;
 import com.nudining.nudining_info.service.UserService;
 import com.nudining.nudining_info.entities.UserLoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.Map;
 
 
 @RestController
@@ -59,4 +62,34 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
 
+    @PutMapping("/updateNutritionFocus/{email}")
+    @CrossOrigin(origins = "http://localhost:5173")
+    public ResponseEntity<User> updateNutritionFocusByEmail(
+            @PathVariable String email,
+            @RequestBody Map<String, Map<String, Double>> focusMap) {
+    
+        Optional<User> userOptional = userService.findUserByEmail(email);
+                
+        System.out.println(userOptional);
+
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    
+        User user = userOptional.get();
+    
+        Map<String, Range> updatedFocus = focusMap.entrySet() //converts to Map.Entry("Nutrient", {min: double, "max": double})
+                                                    .stream() //converts the map.entry to actual elements of an array for iteration
+                                                    .collect(Collectors.toMap( //Convert the stream back into a map
+                                                                    Map.Entry::getKey, 
+                                                                    entry -> new Range(entry.getValue().get("min"), entry.getValue().get("max"))
+                                                            ));
+    
+        user.setNutritionalFocus(updatedFocus); 
+    
+        User updatedUser = userService.saveUser(user);
+    
+        return ResponseEntity.ok(updatedUser);
+    }
+    
 } 
