557382a8f0d632f92b7a7e269d76316cc15c4ac3
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-01-11 19:14:21 -0500

    reimported the data but in a seperate folder that is sibling to the nudining backend folders

---
+++ Data/FetchDailyMenu/api_db.py
@@ -0,0 +1,98 @@
+import requests
+import brotli 
+import json
+import pandas as pd
+
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',  # Indicate that we expect Brotli encoding
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Origin': 'https://nudining.com',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1.1 Safari/605.1.15'
+}
+
+locationID = {
+    "Stetson_East": '586d05e4ee596f6e6c04b527',
+    "Stetson_West": '65ef4de5c625af0775329cb8',
+    "IV": '5f4f8a425e42ad17329be131',
+}
+periods = {
+    "Stetson_East": {
+        "Breakfast": '66b279bae45d4306779faaae',
+        "Lunch": '66b279bae45d4306779faaca',
+        "Dinner": '66b279bae45d4306779faabc',
+        "Everyday": '66b279bae45d4306779faacb',
+    },
+
+    "Stetson_West" : {
+        "Lunch": '6667541f351d530584146022',
+        "Dinner": '6667541f351d53058414602c',
+        "Everyday": '6667541f351d53058414602d'
+    },
+    "IV" : {
+        "Breakfast": '66c3ae9a351d530107802cb1',
+        "Lunch": '66c3ae9a351d530107802cc0',
+        "Dinner": '66c3ae9a351d530107802ccf',
+        "Everyday": '66c3ae9a351d530107802cd0',
+    },
+}
+
+
+
+def generate_url(location_name, period_name):
+    location_id = locationID.get(location_name)
+    period_id = periods.get(location_name, {}).get(period_name)
+    
+    if location_id and period_id:
+        # Construct URL using the selected location and period
+        url = f"https://api.dineoncampus.com/v1/location/{location_id}/periods?platofmr=0&date={period_id}"
+        return url
+    else:
+        print("Invalid location or period")
+        return None
+
+
+
+def fetch_data(location_name, period_name, date):
+    url = generate_url(location_name, period_name)
+    params = {
+        'platform': 0,
+        'date': date,
+    }
+
+    data = None 
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        try:
+            decompressed_data = response.content.decode('utf-8')
+            data = json.loads(decompressed_data) 
+        except ValueError as e:
+            print(f"Error parsing JSON: {e}")
+
+    return data  # Return the fetched data
+
+
+'''
+#Test Purposes
+location_name = "Stetson_East"
+period_name = "Lunch"
+date = '2024-12-5'
+
+filename = f"{location_name}_{period_name}_{date}.json"
+
+data = fetch_data(location_name, period_name, date)
+
+if data is not None:
+    try:
+        with open(filename, 'w') as json_file:  # Open a file for writing
+            json.dump(data, json_file, indent=4)  # Write data to JSON file with indentation
+        print(f"Data has been saved to {filename}.json")
+    except ValueError as e:
+        print(f"Error creating DataFrame: {e}")
+else:
+    print("No data fetched")
+'''
---
+++ Data/FetchDailyMenu/dataConverter.py
@@ -0,0 +1,65 @@
+import json
+import pandas as pd
+from api_db import fetch_data  # Import the function from api-db.py
+
+locations = ["Stetson_East", "Stetson_West", "IV"]
+periods = ["Breakfast", "Lunch", "Dinner"]
+
+# Test Cases
+date = '2025-1-11'
+
+# Combine all dining options into one CSV file
+all_menu_items = []  # List to hold all menu items
+
+for location in locations:
+    for period in periods:
+        try:
+            # Fetch data from the API for each location and period
+            data = fetch_data(location, period, date)
+
+            if data is not None:
+                categories = data["menu"]["periods"]["categories"]
+
+                for kitchen in categories:
+                    kitchen_name = kitchen["name"]
+                
+                    if location == "IV" and kitchen_name == "SOUP":
+                        kitchen_name = "SOUP (IV)"  # Rename SOUP to SOUP (IV) to not conflict with Steast SOUP kitchen as well
+
+                    for item in kitchen["items"]:
+                        item_data = {
+                            "Location": location,
+                            "Period": period,   
+                            "Kitchen": kitchen_name, #The new name is placed here depending whether the kitchen was oreiginally a SOUP or not
+                            "Dish_Name": item["name"],
+                            "Description": item["desc"],
+                            "Portion_Size": item["portion"],
+                            "Ingredients": item["ingredients"],
+                        }
+                        for nutrient in item["nutrients"]:
+                            nutrient_name = nutrient["name"]
+                            nutrient_value = nutrient["value"]
+
+                            # Check for specific phrases and convert to 0.0 if found
+                            if isinstance(nutrient_value, str) and (
+                                "less than 1 gram" in nutrient_value or 
+                                "less than 1 gram+" in nutrient_value or 
+                                "-" in nutrient_value or
+                                "less than 5 milligrams" in nutrient_value
+                            ):
+                                nutrient_value = 0.0  # Convert to 0.0
+                            elif isinstance(nutrient_value, str) and nutrient_value.endswith('+'):
+                                nutrient_value = nutrient_value[:-1]  # Remove the '+' at the end
+
+                            item_data[nutrient_name] = nutrient_value
+
+                        item_data["Allergens"] = ", ".join([f["name"] for f in item["filters"] if f["type"] == "allergen" or f["icon"] == True])
+
+                        all_menu_items.append(item_data)
+
+        except Exception as e:
+            print(f"Error fetching data for {location} during {period}: {e}")
+
+df = pd.DataFrame(all_menu_items)
+
+df.to_csv(f'{date}.csv', index=False)
