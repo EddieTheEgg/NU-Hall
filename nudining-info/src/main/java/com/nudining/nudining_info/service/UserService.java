58836ea026b22f88b93f716398136fae57814e9c
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-03-02 20:55:38 -0500

    removed unecessary autowired from UserService

--- nudining-info/src/main/java/com/nudining/nudining_info/service/UserService.java
+++ nudining-info/src/main/java/com/nudining/nudining_info/service/UserService.java
@@ -2,7 +2,6 @@ package com.nudining.nudining_info.service;
 
 import com.nudining.nudining_info.entities.User;
 import com.nudining.nudining_info.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.nudining.nudining_info.entities.Range;
 
@@ -15,7 +14,6 @@ public class UserService {
     private final UserRepository userRepository;
 
     //UserService Controller(s)
-    @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
